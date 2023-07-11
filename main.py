71442d0d62d4f958cbc477aef80e28f3d76c0abc
thun888 <2238342947@qq.com>
2023-07-11 21:08:38 +0800

    !up

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def parse_layout(file_name, output_dir):
         #print(parsed_content)
         # process the layout tag
         parsed_content += layout(file_name, output_dir, aaa)
-
+        #print(parsed_content)
         # update start position to after the layout tag
         start_pos = match.end()
 
@@ -37,22 +37,58 @@ def parse_layout(file_name, output_dir):
 def layout(file_name, output_dir, aaa):
     file_path = ''
     if aaa == 'ejs':
-        file_path = os.path.join('layout', f"{file_name}.ejs")
+        file_path = os.path.join('layout', f"{file_name}.html")
     elif aaa == 'html':
         file_path = os.path.join('layout','html', f"{file_name}.html")
+    elif aaa == 'value':
+        with open('value.txt', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                parts = line.strip().split()
+                if len(parts) == 2 and parts[0] == file_name:
+                    # print(parts[1])
+                    return parts[1]
+            else:
+                raise ValueError(f'Could not find corresponding line in value.txt for aaa: {file_name}')
     else:
         raise ValueError(f'Invalid layout type: {aaa}')
-    #print(file_path)
-    #print(aaa)
-    with open(file_path, 'r', encoding='utf-8') as f:
+    
+    with open(file_path, 'r', encoding='utf-8') as f:   
         content = f.read()
-    return content
+
+    layout_tag_re = r'<layout\s+([\w\u4e00-\u9fa5]+)\s+(.*?)\s*>'
+
+    parsed_content = ''
+    start_pos = 0
+
+    for match in re.finditer(layout_tag_re, content):
+        #print(filename)
+        file_name = match.group(2)
+        aaa = match.group(1)
+        #print(file_name)
+        #print(aaa)
+
+        # process content before the layout tag
+        parsed_content += content[start_pos:match.start()]
+        #print(parsed_content)
+        # process the layout tag
+        parsed_content += layout(file_name, output_dir, aaa)
+        #print(parsed_content)
+        # update start position to after the layout tag
+        start_pos = match.end()
+
+    # process content after the last layout tag
+    parsed_content += content[start_pos:]
+
+    return parsed_content
+
+
 
 def output(filename,content):
     file_name = 'index.html'
-    if filename == 'index.ejs':
+    if filename == 'index.html':
         output_dir = os.path.join('dist')
-    elif filename == '404.ejs':
+    elif filename == '404.html':
         output_dir = os.path.join('dist')
         file_name = '404.html'
     else:
@@ -70,7 +106,7 @@ if __name__ == '__main__':
     layout_dir = 'layout'
     for dirpath, dirnames, filenames in os.walk(layout_dir):
         for filename in tqdm(filenames, desc='Processing layout files'):
-            if filename.endswith('.ejs'):
+            if filename.endswith('.html'):
                 file_path = os.path.join(dirpath, filename)
                 output_dir = os.path.join('dist', os.path.relpath(dirpath, layout_dir))
                 parse_layout(file_path, output_dir)
